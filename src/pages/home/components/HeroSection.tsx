caa1597dc4870b1073b509439703ecc7ab5454c8
grabbedbyalienspayments-eng <grabbedbyalienspayments@gmail.com>
2026-01-07 17:58:52 +0200

    Modify scrollToSection to include offset
    Adjust scroll behavior to account for offset.

--- src/pages/home/components/HeroSection.tsx
+++ src/pages/home/components/HeroSection.tsx
@@ -5,7 +5,11 @@ const HeroSection = () => {
   const scrollToSection = (sectionId: string) => {
     const element = document.getElementById(sectionId);
     if (element) {
-      element.scrollIntoView({ behavior: 'smooth' });
+      const offsetTop = element.offsetTop - 80;
+      window.scrollTo({
+        top: offsetTop,
+        behavior: 'smooth'
+      });
     }
   };
 
